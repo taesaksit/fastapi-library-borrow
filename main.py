8172cd3087759909024f98fd41c0aa6f6fcb2acd
SAKSIT FROM PC <taesaksit09@gmail.com>
2025-07-27 13:23:22 +0700

    config cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from models.user import User
 from config.database import Base, engine
@@ -9,6 +10,15 @@ from routes import auth, category, book, borrow
 
 app = FastAPI()
 
+# CORS middleware configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 Base.metadata.create_all(bind=engine)
 app.add_exception_handler(HTTPException, http_exception_handler)
 
@@ -16,6 +26,7 @@ app.include_router(auth.router, prefix="/api")
 app.include_router(category.router, prefix="/api")
 app.include_router(book.router, prefix="/api")
 app.include_router(borrow.router, prefix="/api")
+
 
 @app.get("/")
 def root(current_user: User = Depends(get_current_user)):
